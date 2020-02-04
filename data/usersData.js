8ab886d136e7e0a3532fd1a9cda16e0993efbc58
Allison Wagner <allisonjw01@gmail.com>
2020-02-04 13:40:44 -0700

    Adds data folder with users, projects and palettes

---
+++ data/usersData.js
@@ -0,0 +1,14 @@
+const usersData = [
+    { 
+        name: 'Jessie', 
+        password: 'Jessie', 
+        id: 1 
+    },
+    { 
+        name: 'Allison', 
+        password: 'Allison', 
+        id: 2 
+    }
+];
+
+module.exports = usersData;
