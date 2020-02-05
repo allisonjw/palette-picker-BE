7105c06046c8736aaec0ed8fc243f4e9099d8385
Allison Wagner <allisonjw01@gmail.com>
2020-02-05 13:16:31 -0700

    Changes name to match altered name in database

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/palette_tests',
+    connection: 'postgres://localhost/all_palette_test',
     migrations: {
       directory: './db/migrations'
     },
