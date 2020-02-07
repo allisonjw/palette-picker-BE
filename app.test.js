7b207052be559115d285888f97c5108edccc28eb
Jessie Le-Ho <lehoj@oregonstate.edu>
2020-02-06 17:52:38 -0700

    Get all palettes and projects back, along with getting back individual palettes and projects

--- app.test.js
+++ app.test.js
@@ -16,19 +16,73 @@ describe('Server', () => {
             expect(response.status).toBe(200)
         });
     });
-    describe('GET /api/v1/palettes', () => {
-        it('should return a status code of 200 and all the palettes', async () => {
-            //SET UP
-            //dealing with database itself
-            const expectedPalettes = await database('palettes').select();
-            //now super test comes into play
-            //EXECUTION
-            const response = await request(app).get('/api/v1/palettes');
-            const palettes = response.body //to see if we get the response we want back
-            //ASSERTION
-            expect(response.status).toBe(200); //HAPPY PATH
-            expect(palettes).toEqual(expectedPalettes);
-            //now implement the route in the server file aka app.js
+    describe('Palettes', () => {
+        describe('GET /api/v1/palettes', () => {
+            it('should return a status code of 200 and all the palettes', async () => {
+                //SET UP
+                //dealing with database itself
+                const expectedPalettes = await database('palettes').select();
+                //now super test comes into play
+                //EXECUTION
+                const response = await request(app).get('/api/v1/palettes');
+                const palettes = response.body //to see if we get the response we want back
+                //ASSERTION
+                expect(response.status).toBe(200); //HAPPY PATH
+                expect(palettes).toEqual(expectedPalettes);
+                //now implement the route in the server file aka app.js
+            })
+        });
+        describe('GET /api/v1/palettes/:id', () => {
+            it('should return a code of 200 and a single palette if the palette exists', async () => {
+                const expectedPalette = await database('palettes').first();
+                const { id } = expectedPalette;
+                const response = await request(app).get(`/api/v1/palettes/${id}`);
+                const result = response.body[0];
+                expect(response.status).toBe(200);
+                expect(result).toEqual(expectedPalette);
+            });
+            it('should return a 404 and "palette not found" if the resource is not there', async () => {
+                const invalidId = -1;
+    
+                const response = await request(app).get(`/api/v1/palettes/${invalidId}`)
+                expect(response.status).toBe(404);
+                expect(response.body.error).toEqual('palette not found');
+            })
+        })
+
+    });
+    describe('Projects', () => {
+        describe('GET /api/v1/projects', () => {
+            it('should return a status code of 200 and all the projects', async () => {
+                //SET UP
+                //dealing with database itself
+                const expectedProjects = await database('projects').select();
+                //now super test comes into play
+                //EXECUTION
+                const response = await request(app).get('/api/v1/projects');
+                const projects = response.body //to see if we get the response we want back
+                //ASSERTION
+                expect(response.status).toBe(200); //HAPPY PATH
+                expect(projects).toEqual(expectedProjects);
+                //now implement the route in the server file aka app.js
+            })
+        });
+        describe('GET /api/v1/projects/:id', () => {
+            it('should return a code of 200 and a single project if the project exists', async () => {
+                const expectedProject = await database('projects').first();
+                const { id } = expectedProject;
+                const response = await request(app).get(`/api/v1/projects/${id}`);
+                const result = response.body[0];
+                expect(response.status).toBe(200);
+                expect(result).toEqual(expectedProject);
+            });
+            it('should return a 404 and "palette not found" if the resource is not there', async () => {
+                const invalidId = -1;
+    
+                const response = await request(app).get(`/api/v1/palettes/${invalidId}`)
+                expect(response.status).toBe(404);
+                expect(response.body.error).toEqual('palette not found');
+            })
         })
     })
     
